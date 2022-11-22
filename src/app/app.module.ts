6dbc555a6db3d9d42f63bf0db80717642ae7865e
ldelfabro <delfabro_leandro@hotmail.com>
2022-11-22 19:15:02 -0300

    Segundo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiSegundoComponenteComponent } from './clase1/mi-segundo-componente/mi-segundo-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiSegundoComponenteComponent
   ],
   imports: [
     BrowserModule,
